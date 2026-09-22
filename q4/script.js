4bccd0176f8155c2d3be8bb53409fd940421c244
Rama khaldoun <ramakh.hamdann@gmail.com>
2026-09-22 20:05:00 +0300

    Create script.js

---
+++ q4/script.js
@@ -0,0 +1,9 @@
+let num1=prompt("enter first number: ");
+let num2=prompt("enter second number: ");
+
+if(num1>num2){
+    alert(num1 + ": " + "is larger");
+}
+else if(num2>num1){
+       alert(num2 + ": " + "is larger");
+}
