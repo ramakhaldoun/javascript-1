486f657e000a4778ae8117cc7a1fb1a251c8e0bc
Rama khaldoun <ramakh.hamdann@gmail.com>
2026-09-22 20:04:08 +0300

    Create script.js

---
+++ q3./script.js
@@ -0,0 +1,9 @@
+let number1=prompt("enter the first number: ");
+let number2=prompt("enter the second number: ");
+
+if (number1>number2){
+    alert("Ascending order: " + number1 + "," + number2);
+}
+else if (number2>number1){
+     alert("Ascending order: " + number2 + "," + number1);
+}
