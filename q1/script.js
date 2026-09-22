8ebb05ad5389c3cbb5f96ef090ec731240b7238d
Rama khaldoun <ramakh.hamdann@gmail.com>
2026-09-22 19:58:46 +0300

    Create script.js

---
+++ q1/script.js
@@ -0,0 +1,33 @@
+console.log(-9*3);
+console.log("value is" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4 == 4);
+console.log(4 != 5);
+console.log(7 <= 8);
+
+let x=4.3;
+console.log(Math.ceil(x) - Math.floor(x));
+
+console.log(typeof(100));
+console.log(typeof(73.9));
+console.log(typeof(NaN));
+console.log(typeof("Water"));
+console.log(typeof(false));
+console.log(typeof(9 != 11));
+console.log("Orang" + "e");
+console.log("Orange" - "s");
+console.log("4"+"8");
+console.log("4"-"8");
+console.log("name" + 3);
+console.log("name" - 3); //NaN
+console.log(82 * "word"); //NaN
+console.log(1 + "hello");
+console.log("hello" + 1);
+console.log(1 + true); //2
+console.log("hello" + true); //hellotrue
+console.log(typeof(Infinity)); //number
+console.log(1 == '1'); //true
+console.log(1 === '1'); //false
+
